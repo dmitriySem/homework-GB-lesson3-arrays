63c5fabc7ab5b0102248d6098a439f39aee46081
Dmitriy Semenov <dsemenov@saec.ru>
2023-11-29 22:56:14 +0500

    первое задание

--- Program.cs
+++ Program.cs
@@ -1,2 +1,27 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+//Задача 1: Задайте одномерный массив из 10 целых чисел от 1 до 100. 
+//Найдите количество элементов массива, значения которых лежат в отрезке [20,90].
+/*
+Console.Clear();
+Random rnd =  new Random();
+
+int[] array = new int[10];
+
+for(int i=0; i<=9;i++){
+
+    array[i] = rnd.Next(1,100);
+}
+
+foreach(int el in array)
+    Console.Write($" {el}");
+
+int k = 0;
+foreach(int el in array)
+    if (el>=20 & el<=90)
+        k++;
+
+Console.WriteLine($" => количество чисел лежащих в отрезке [20,90]: {k}");
+*/
+
+
+
